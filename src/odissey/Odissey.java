3a9b1f04acf295503247ef505d503ce9b31fff31
Maesly <nadieska23@outlook.es>
2018-05-17 16:08:18 -0600

    [UI][Logic]: Toma datos de la interfaz

--- src/odissey/Odissey.java
+++ src/odissey/Odissey.java
@@ -4,19 +4,20 @@ package odissey;
  *
  * @author maesly
  */
-
 import odissey.VentanaPrincipal;
+
 public class Odissey {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
         VentanaPrincipal ventana = new VentanaPrincipal();
         ventana.setResizable(false);
         ventana.setVisible(true);
-        
+
+
     }
     
 }
